f6a52dac869c04973a034e9e4b288a23f2bbc7b7
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2026-02-02 23:02:11 -0800

    feat: initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+name 'cbl-weapons'
+author 'Venoxity Development'
+version '0.0.0'
+game 'gta5'
+lua54 'yes'
+fx_version 'cerulean'
+
+client_scripts {
+    'client/**/*.lua'
+}
+
+server_scripts {
+    'server/**/*.lua'
+}
